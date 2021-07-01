1329ede46aaf26f57961c448cd9bd61ae558381a
joeldevel <christianjoel.developer@gmail.com>
2021-07-01 00:08:24 -0300

    TOP->fundamentals->part-4:removeFromArray tests passed

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+  let items = Array.prototype.slice.call(arguments, 1);
+  let outputArray = [];
+  let shouldBeRemoved = false;
+  for(let i=0; i< arr.length; i++) {
+    for(let j=0; j<items.length; j++){
+      if(arr[i] === items[j]) {
+        shouldBeRemoved = true;
+        break;
+      } else {
+        shouldBeRemoved = false;
+      }
+    } // end for
+    // console.log('is shouldBeRemoved?',arr[i], shouldBeRemoved);
+    if( !shouldBeRemoved) {
+      outputArray.push(arr[i]);
+    }
+  }// end for
+  // console.log({items});
+  // console.log({outputArray});
+  return outputArray;
 };
 
 module.exports = removeFromArray;
