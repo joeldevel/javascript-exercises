e4ba1414d351ce259d8bc2b79f455c04ce7a9532
joeldevel <christianjoel.developer@gmail.com>
2021-07-01 07:58:24 -0300

    TOP->fundamentals->part-4:tempConversion tests OK

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(t) {
+  return +((t-32.0) * 5.0/9.0).toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(t) {
+  return +(t* 9.0/5.0 + 32.0).toFixed(1);
 };
 
 module.exports = {
