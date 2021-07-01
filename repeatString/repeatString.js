69dc3663d129006a8039257a2783482ddf1d1938
joeldevel <christianjoel.developer@gmail.com>
2021-06-30 21:40:42 -0300

    TOP->fundamentals->part-4:reverseString tests passed

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+  if(times < 0) return "ERROR";
+  let result = '';
+  for(let i=0; i< times; i++) {
+    result += str;
+  }
+  return result;
 };
 
 module.exports = repeatString;
