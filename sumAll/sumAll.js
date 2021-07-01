cbaf04608dc0985540f6d90fe49bb1701bd4d797
joeldevel <christianjoel.developer@gmail.com>
2021-07-01 07:31:30 -0300

    TOP->fundamentals->part-4:sumAll tests ok

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+  let sum = 0;
+  if( x<0 || y<0 ||
+     typeof x != 'number'|| typeof y != 'number' ) return 'ERROR';
+  let aux = x;
+  if(x > y) {
+    x = y;
+    y = aux;
+  }
+  for(let i=x; i<=y; i++) {
+    sum +=i;
+  }
+  return sum;
 };
 
 module.exports = sumAll;
