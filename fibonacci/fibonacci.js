06ed4cf842b0812a0b3c9ccd5781fd4973fe7986
joeldevel <christianjoel.developer@gmail.com>
2021-09-15 20:03:41 -0300

    fibonacci passes tests

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(Number(n)) {
+	if(n < 0) return "OOPS";
+	if(n < 2) return n;
+	return fibonacci(n-1) + fibonacci(n-2);
 };
 
 module.exports = fibonacci;
