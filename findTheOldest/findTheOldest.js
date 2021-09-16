6bfd63e69416b1a3f8cec6b542a2afea39f0814b
joeldevel <christianjoel.developer@gmail.com>
2021-09-15 21:50:42 -0300

    pass findTheOldest tests

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+	const reducer = (birth, death=2021) => death - birth;
+	const ages = people.map((p)=> reducer(p.yearOfBirth, p.yearOfDeath));
+	ages.sort();
+	return people.find(p => {
+	  const d = p.yearOfDeath ? p.yearOfDeath : 2021;
+		return (d - p.yearOfBirth === ages[ages.length-1]);
+	});
 };
 
 module.exports = findTheOldest;
